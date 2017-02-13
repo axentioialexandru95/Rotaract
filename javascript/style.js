84ffe11889ef6ac85ef76b7f56cdab3f82dc6942
Alexandru <axentioialexandru95@gmail.com>
2017-02-13 18:25:19 +0200

    Homepage Layout Finished

--- javascript/style.js
+++ javascript/style.js
@@ -1,1 +1,24 @@
-style.js
+$(document).ready(function() {
+
+AOS.init();
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 10) {
+        $(".navbar").addClass('big');
+    }
+    else {
+        $(".navbar").removeClass('big');
+    }
+});
+
+$('.slider').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+
+});
